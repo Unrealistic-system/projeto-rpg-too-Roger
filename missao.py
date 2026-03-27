1bfce53ad98924536f5ab7c73a300de205c6325c
roger <rogerandrecampos@gmail.com>
2026-03-27 00:54:24 -0300

    missao atualizando erros

--- missao.py
+++ missao.py
@@ -1,14 +1,30 @@
 class missao:
-    def __init__(self, nome: str, descricao:str, rec:int, stat:str = "PENDENTE"):
+    def __init__(self, nome, descricao, recompensa):
         self.nome = nome
         self.descricao = descricao
-        self.recompensa = rec
-        self.status = stat
+        self.recompensa = recompensa
+        self.status = "PENDENTE"
 
-    #PROPERTY's -> esse é o getter
-    @property
+        self.nome = nome
+        self.descricao = descricao
+        self.recompensa = recompensa
+
+    #PROPERTY's -> esse é o getter, pra poder receber de volta os dados
+    #SETTER's -> é como gravar os dados do objeto
+    # NOME:
+    @property 
     def nome(self):
         return self._nome
+    @nome.setter
+    def nome(self, novo_nome):
+        if not isinstance(novo_nome, str):
+            raise TypeError ("Nome precisa ser texto!!!")
+        novo_nome = novo_nome.split()# separa
+        novo_nome = ' '.join(novo_nome)# junta sem espaço a mais
+        if not novo_nome: # pega qualquer coisa, " " ou none tmb
+            raise ValueError ("Nome é obrigatório!!!")
+        self._nome = novo_nome
+        
     @property
     def descricao(self):
         return self._descricao
@@ -19,16 +35,6 @@ class missao:
     def status(self):
         return self._status
     
-    #SETTER's
-    @nome.setter
-    def nome(self, novo_nome):
-        if not isinstance(novo_nome, str):
-            raise TypeError ("Nome precisa ser texto!!!")
-        novo_nome = novo_nome.split()# separa
-        novo_nome = ' '.join(novo_nome)# junta sem espaço a mais
-        if not novo_nome: # pega qualquer coisa, " " ou none tmb
-            raise ValueError ("Nome é obrigatório!!!")
-        self._nome = novo_nome
     @descricao.setter
     def descricao(self, n_desc):
         if not isinstance(n_desc, str):
@@ -38,6 +44,7 @@ class missao:
         if  not n_desc:
             raise ValueError ("Descrição é obrigatória!!!")
         self._descricao = n_desc
+
     @recompensa.setter
     def recompensa(self, n_rec):
         if not isinstance(n_rec, int):
@@ -45,6 +52,7 @@ class missao:
         if 0 <= n_rec > 50:
             raise Exception ("Recompensa precisa ser positiva e menor que 50!!!")
         self._recompensa = n_rec
+
     @status.setter
     def status(self, n_st):
         if not isinstance(n_st, str):
@@ -55,14 +63,15 @@ class missao:
             self._status = n_st
         else:
             raise ValueError(f"Status inválido! Use uma destas opções: {st_posssivel}")
-    '''
-    SAIDAS:
-    '''
+
+
     def exibir_dados(self):
         return f"{'='*30}\n--- MISSÃO ---\nNome da Missão: {self.nome}\nDescrição: {self.descricao}\nRecompensa: {self.recompensa} XP\nStatus: {self.status}\n{'='*30}"# posso usar contas pra exibir varios caracteres iguais :D
-       
+
+
     def __str__(self):
         return f"{self.nome} ({self.descricao}) [{self.recompensa}] [{self.status}]"
+   
     def __eq__(self, outro:object) -> bool:
         if not isinstance(outro, missao):
             return False
