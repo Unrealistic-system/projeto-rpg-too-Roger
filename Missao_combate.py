e091178b2c79dcd47eccdc67466cf07199f61ed2
roger <rogerandrecampos@gmail.com>
2026-03-30 08:54:26 -0300

    missao combate pronta

--- Missao_combate.py
+++ Missao_combate.py
@@ -10,7 +10,6 @@ class MissaoCombate (Missao):
     @property 
     def inimigo(self):
         return self._inimigo
-    
     @inimigo.setter
     def inimigo(self, it):
         if not isinstance(it, str):
@@ -19,6 +18,7 @@ class MissaoCombate (Missao):
         it = ' '.join(it)
         it = it.title() #maiuscula primeira
         self._inimigo = it
+
     @property
     def inimigos_a_derrotar(self):
         return self._inimigos_a_derrotar
@@ -26,4 +26,27 @@ class MissaoCombate (Missao):
     def inimigos_a_derrotar(self, qt):
         if not isinstance(qt, int):
             raise TypeError("Inimigos a derrotar precisa ser inteiro.")
+        self._inimigos_a_derrotar = qt
         
+    
+    def exibir_dados(self):
+        return (f"{'='*30}\n--- MISSÃO DE COMBATE: ---"
+                f"\nNome da Missão: {self.nome}\n"
+                f"Descrição: {self.descricao}\n"
+                f"Recompensa: {self.recompensa} XP\n"
+                f"Status: {self.status.name}\n"
+                f"Inimigo : {self.inimigo}\n"
+                f"Inimigos a derrotar: {self.inimigos_a_derrotar}\n{'='*30}")
+
+    def __str__(self):
+        return (f"{self.nome} ({self.descricao}) XP:[{self.recompensa}]"
+                f"[{self.status.value}], inimigo: {self.inimigo} X [{self.inimigos_a_derrotar}]")
+   
+    def __eq__(self, outro:object) -> bool:
+        if not isinstance(outro, MissaoCombate):
+            return False
+        return (self.nome == outro.nome and self.descricao == outro.descricao 
+                and self.recompensa == outro.recompensa and self.status == outro.status
+                and self.inimigo == outro.inimigo 
+                and self.inimigos_a_derrotar == outro.inimigos_a_derrotar)
+    
